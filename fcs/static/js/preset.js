6d7a9a30647d91d68b781a0514a2a7e893816067
Perfection Loveday <59074379+Samperfect@users.noreply.github.com>
2021-01-05 18:19:22 +0100

    Update preset.js

--- fcs/static/js/preset.js
+++ fcs/static/js/preset.js
@@ -41,7 +41,7 @@ displaySwitch.addEventListener('click', () => {
 
 //returning a message when the search icon is clicked
 search.addEventListener('click', function (event) {
-  
+  alert('shit');
 
   halfmoon.initStickyAlert({
     content:
